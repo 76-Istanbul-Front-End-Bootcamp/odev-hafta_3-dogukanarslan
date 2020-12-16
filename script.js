9a39bf9b8c3cbba8877cab6b1822783f6cfb5e4a
Doğukan Arslan <dogukan2arslan@hotmail.com>
2020-12-16 21:55:18 +0000

    Refactor prototype with class

--- script.js
+++ script.js
@@ -1,52 +1,63 @@
-function Animal(name){
+class Animal {
+
+  constructor(name) {
     this.name = name;
-}
+  }
 
-Animal.prototype.action = function(){
-  document.getElementById(this.actionSoundName).play();
-  
-}
+  action(event) {
+    event.stopPropagation();
+    document.getElementById(this.actionSoundName).play();
+  }
 
-Animal.prototype.putInTheDocument = function(){
+  addImg() {
+    document.getElementById("boxImg").src = this.url;
+  }
+
+  putInTheDocument() {
     var petsTable = document.getElementById("petsTable");
     var petTR = document.createElement("tr");
-  
+    var boxImg = document.getElementById("boxImg");
+    petTR.addEventListener("click", this.addImg)
+
     var petNameTD = document.createElement("td");
     petNameTD.textContent = this.name;
     petTR.appendChild(petNameTD);
-  
+
     var petLegsTD = document.createElement("td");
     petLegsTD.textContent = this.legs;
     petTR.appendChild(petLegsTD);
-  
+
     var petActionTD = document.createElement("td");
     var petActionTDButton = document.createElement("button");
     petActionTDButton.textContent = this.actionText;
     petActionTD.appendChild(petActionTDButton);
     petTR.appendChild(petActionTD);
-    
+
     petActionTDButton.onclick = this.action.bind(this);
+    petTR.onclick = this.addImg.bind(this);
     petsTable.querySelector("tbody").appendChild(petTR)
+  }
 }
 
-
-function Cat(name){
-    Animal.call(this, name);
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
     this.legs = 4;
-    this.actionText = "Meoow"
-    this.actionSoundName = "meow"
+    this.actionText = "Meoow";
+    this.actionSoundName = "meow";
+    this.url = "https://i.pinimg.com/600x315/ce/37/8a/ce378abc8dba1d55ccd37902337900ed.jpg";
+  }
 }
 
-Cat.prototype = Animal.prototype;
-
-function Monkey(name){
-    Animal.call(this, name);
+class Monkey extends Animal{
+  constructor(name) {
+    super(name);
     this.legs = 2;
     this.actionText = "Scream";
     this.actionSoundName = "scream";
+    this.url = "https://i.pinimg.com/originals/bd/9d/59/bd9d598b56988f8c8cd48be6e116a60b.png";
+  }
 }
-
-Monkey.prototype = Animal.prototype;
 
 var Mila = new Cat("Mila");
 Mila.putInTheDocument();
